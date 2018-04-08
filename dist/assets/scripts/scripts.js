66f65a1ec5944ce274364e56a76caa08d35b1429
Sergey Glazov <yaglazov@gmail.com>
2018-04-08 18:41:17 +0300

    добавил штуку для дат

--- dist/assets/scripts/scripts.js
+++ dist/assets/scripts/scripts.js
@@ -1,4 +1,16 @@
-"use strict";
+'use strict';
+
+$(document).ready(function () {
+  moment.locale('ru');
+  $('input[name="daterange"]').daterangepicker({
+    "startDate": "09/4/2018",
+    "endDate": "10/5/2018",
+    locale: {
+      cancelLabel: 'Сбросить',
+      applyLabel: 'Применить'
+    }
+  });
+});
 "use strict";
 
 function AddReadMore() {
